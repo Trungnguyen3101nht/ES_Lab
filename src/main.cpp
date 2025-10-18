2ccd562eb0a407d5c0dd7910c42bdf24b938ec1b
Trung <trung.nguyenhaitrung31@gmail.com>
2025-10-18 20:58:27 +0700

    update Lab 2

--- src/main.cpp
+++ src/main.cpp
@@ -25,12 +25,15 @@ void acyclicTask(void *parameter)
     Serial.println("ESP32");
   }
 }
-
+// vì cần sử lí ngay lập tức mà nếu lưu tròn flash memory thì có thể bị miss trong một số tính huống(như truy cập SPIflash) nên lưu trong RAM sẽ an toàn và thực thi ổn định hơn
 void IRAM_ATTR handleButtonInterrupt()
 {
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  // phân biệt xem task nào có độ ưu tiên cao hơn để thực hiện sau khi thực hiện ngắt
   vTaskNotifyGiveFromISR(acyclicTaskHandle, &xHigherPriorityTaskWoken);
+  // gửi tín hiệu ngắn đến task acyclicTask đê nó thực thi, nếu task có độ ưu tiên cao hơn task đang thực thi, gán pdTRUE
   portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+  // đảm bảo task có độ ưu tiên cao hơn được thực thi ngay lập tức sau khi ngắt xảy ra
 }
 
 void setup()
@@ -53,8 +56,13 @@ void setup()
       NULL,
       2,
       &acyclicTaskHandle);
-
+  // giúp  nhận nút nhấn ngay mà ko cần phải kiểm tra liên tục trong loop
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), handleButtonInterrupt, FALLING);
+  /*
+  - digitalPinToInterrupt(BUTTON_PIN) → Lấy số ngắt tương ứng với chân đó.
+  - handleButtonInterrupt → Hàm sẽ được gọi ngay lập tức khi ngắt xảy ra.
+  - FALLING → Kiểu ngắt: xảy ra khi tín hiệu chuyển từ mức cao xuống mức thấp (nút được nhấn trong trường hợp dùng INPUT_PULLUP).
+  */
 }
 
 void loop()

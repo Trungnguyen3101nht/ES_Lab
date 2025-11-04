493842feeddebdba568357194815e63d554af236
Trung <trung.nguyenhaitrung31@gmail.com>
2025-11-04 16:22:42 +0700

    update Lab3

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,62 @@
 #include <Arduino.h>
-
 // Prioritized Pre-emptive Scheduling with Time Slicing.///
 
 // TaskHandle_t Task1, Task2;
 
-// void task1(void *pv) {
-//   while (1) {
+// void task1(void *pv)
+// {
+//   while (1)
+//   {
 //     Serial.println("Task 1 running");
 //     vTaskDelay(pdMS_TO_TICKS(100));
 //   }
 // }
 
-// void task2(void *pv) {
-//   while (1) {
+// void task2(void *pv)
+// {
+//   while (1)
+//   {
 //     Serial.println("Task 2 running");
 //     vTaskDelay(pdMS_TO_TICKS(100));
 //   }
 // }
 
-// void setup() {
+// void setup()
+// {
 //   Serial.begin(115200);
 //   xTaskCreate(task1, "Task1", 2048, NULL, 1, &Task1);
 //   xTaskCreate(task2, "Task2", 2048, NULL, 1, &Task2);
-
+// }
+// void loop() {}
 //-----------------------------------------------------------------------------
 // Prioritized Pre-emptive Scheduling without Time Slicing. ///
-// #define configUSE_TIME_SLICING 0
-
-// void highPriorityTask(void *pv) {
-//   while (1) {
+// void highPriorityTask(void *pv)
+// {
+//   while (1)
+//   {
 //     Serial.println("High Priority Task running");
-//     vTaskDelay(pdMS_TO_TICKS(1000));
+//     vTaskDelay(pdMS_TO_TICKS(3000));
 //   }
 // }
 
-// void lowPriorityTask(void *pv) {
-//   while (1) {
+// void lowPriorityTask(void *pv)
+// {
+//   while (1)
+//   {
 //     Serial.println("Low Priority Task running");
 //     vTaskDelay(pdMS_TO_TICKS(1000));
 //   }
 // }
 
-// void setup() {
-//   Serial.begin(115200);
-
-//   xTaskCreate(highPriorityTask, "High", 2048, NULL, 2, NULL);
-//   xTaskCreate(lowPriorityTask, "Low", 2048, NULL, 1, NULL);
-// }
-
-// void loop()
+// void setup()
 // {
+//   Serial.begin(115200);
+//   // Task high có priority cao hơn → sẽ luôn được ưu tiên chạy
+//   xTaskCreatePinnedToCore(highPriorityTask, "High", 2048, NULL, 2, NULL, 1);
+//   xTaskCreatePinnedToCore(lowPriorityTask, "Low", 2048, NULL, 1, NULL, 1);
 // }
+
+// void loop() {}
 
 //-----------------------------------------------------------------------------
 // Co-operative Scheduling
@@ -58,8 +64,8 @@
 // {
 //   while (1)
 //   {
-//     Serial.println("Task run...");
-//     taskYIELD();
+//     Serial.println("Task 1 run...");
+//     vTaskDelay(1000 / portTICK_PERIOD_MS);
 //   }
 // }
 
@@ -68,7 +74,7 @@
 //   while (1)
 //   {
 //     Serial.println("Task 2 run...");
-//     taskYIELD();
+//     vTaskDelay(1000 / portTICK_PERIOD_MS);
 //   }
 // }
 
@@ -78,34 +84,4 @@
 //   xTaskCreate(coopTask1, "Coop1", 2048, NULL, 1, NULL);
 //   xTaskCreate(coopTask2, "Coop2", 2048, NULL, 1, NULL);
 // }
-
-//-----------------------------------------------------------------------------
-// extra exercise
-
-volatile uint32_t idleCounter = 0;
-uint32_t lastMillis = 0;
-
-extern "C" void vApplicationIdleHook(void)
-{
-  idleCounter++;
-}
-
-void setup()
-{
-  Serial.begin(115200);
-}
-
-void loop()
-{
-  vTaskDelay(pdMS_TO_TICKS(1000));
-  uint32_t now = millis();
-  static uint32_t lastCount = 0;
-
-  uint32_t deltaCount = idleCounter - lastCount;
-  lastCount = idleCounter;
-
-  float cpuIdlePercent = (deltaCount / 10000.0) * 100;
-  float cpuUsage = 100.0 - cpuIdlePercent;
-
-  Serial.printf("CPU Usage: %.2f%%\n", cpuUsage);
-}
+// void loop() {}
